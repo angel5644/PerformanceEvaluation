0b6969e7c7a6a7c63cba09a98746347c6fe2bfd2
Carlos Adan Urzua Romero <caurza@gmail.com>
2018-10-15 14:26:23 -0500

    #204 - Creation of view model file

---
+++ PES/PES/ViewModels/ReadOnlyUserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PES.ViewModels
+{
+    public class ReadOnlyUserViewModel
+    {
+        public string EmployeeFirstName { get; set; }
+
+        public string EmployeeLastName { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; } 
+
+        public DateTime ReturnDate { get; set; }
+
+        public string StatusOfRequest { get; set; }
+
+
+
+    }
+}
