978b448d3bd61805ec8c2c590849e93369eb1389
Daniel <Daniel_112@live.com.mx>
2018-10-19 13:32:31 -0500

    #209 - ViewModel created

---
+++ PES/PES/ViewModels/RequestDetailsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PES.ViewModels
+{
+    public class RequestDetailsViewModel : ReadOnlyUserViewModel
+    {
+        public string EmployeePosition { get; set; }
+
+        public string EmployeeProfile { get; set; }
+
+        public string EmployeeLocation { get; set; }
+
+        public int EmployeeFreeDays { get; set; }
+
+        public int ManagerID { get; set; }
+
+        public string ManagerPosition { get; set; }
+
+        public int RequestID { get; set; }
+
+        public string Comments { get; set; }
+
+        public int UnpaidDays { get; set; } = 0;
+    }
+}
