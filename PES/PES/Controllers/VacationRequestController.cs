1536a31374729540a0ebad797c93d879a36d22ea
David Francisco Ruiz Navarro <druiz@ucol.mx>
2018-10-31 13:32:00 -0600

    #213 - Vacation Request Controller Modified => S8 - Date Created value added

--- PES/PES/Controllers/VacationRequestController.cs
+++ PES/PES/Controllers/VacationRequestController.cs
@@ -856,37 +856,39 @@ namespace PES.Controllers
 
 			if (currentUser.ProfileId == Convert.ToInt32(ProfileUser.Manager))
 			{
-				listHeaderReqDB = _headerReqService.GetAllGeneralVacationHeaderReqByManagerId(currentUser.EmployeeId);
+				listHeaderReqDB = _headerReqService.GetAllGeneralVacationHeaderReqByManagerId(currentUser.EmployeeId).ToList();
 			}
 			else if (currentUser.ProfileId == Convert.ToInt32(ProfileUser.Resource))
 			{
-                listHeaderReqDB = _headerReqService.GetGeneralVacationHeaderReqByEmployeeId(currentUser.EmployeeId);
-			}
-			if (listHeaderReqDB != null && listHeaderReqDB.Count > 0)
-			{
-				foreach (var headerReq in listHeaderReqDB)
-				{
-					var headerReqVm = new VacHeadReqViewModel
-					{
-						VacationHeaderReqId = headerReq.VacationHeaderReqId,
-						EmployeeId = headerReq.EmployeeId,
-						Title = headerReq.Title,
-						NoVacDays = headerReq.NoVacDays,
-						status = _ReqStatusService.GetVacationReqStatusById(headerReq.ReqStatusId).Name,
-						StartDate = headerReq.StartDate,
-						EndDate = headerReq.EndDate,
-						ReturnDate = headerReq.ReturnDate,
-						FirstName = headerReq.FirstName,
-						LastName = headerReq.LastName,
-						HaveProject = headerReq.HaveProject,
-
-
-					};
-					listHeaderReqVM.Add(headerReqVm);
-				}
+                listHeaderReqDB = _headerReqService.GetGeneralVacationHeaderReqByEmployeeId(currentUser.EmployeeId).ToList();
 			}
 
-			ViewBag.Username = currentUser.FirstName + " " + currentUser.LastName;
+            if (listHeaderReqDB != null && listHeaderReqDB.Count > 0)
+            {
+                foreach (var headerReq in listHeaderReqDB)
+                {
+                    var headerReqVm = new VacHeadReqViewModel
+                    {
+                        VacationHeaderReqId = headerReq.VacationHeaderReqId,
+                        EmployeeId = headerReq.EmployeeId,
+                        Title = headerReq.Title,
+                        NoVacDays = headerReq.NoVacDays,
+                        status = _ReqStatusService.GetVacationReqStatusById(headerReq.ReqStatusId).Name,
+                        StartDate = headerReq.StartDate,
+                        EndDate = headerReq.EndDate,
+                        ReturnDate = headerReq.ReturnDate,
+                        FirstName = headerReq.FirstName,
+                        LastName = headerReq.LastName,
+                        HaveProject = headerReq.HaveProject,
+                        CreationDate = headerReq.CreationDate,
+
+
+                    };
+                    listHeaderReqVM.Add(headerReqVm);
+                }
+            }
+
+            ViewBag.Username = currentUser.FirstName + " " + currentUser.LastName;
 			ViewBag.UserID = currentUser.EmployeeId;
 			ViewBag.FreeDays = currentUser.Freedays;
 			ViewBag.profileId = currentUser.ProfileId;
