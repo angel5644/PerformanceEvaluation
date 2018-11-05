ebf0dd75d22e35f6f1605203c3977b1cbeacd855
Daniel <Daniel_112@live.com.mx>
2018-11-05 09:36:08 -0600

    #217 - ViewModel

---
+++ PES/PES/ViewModels/Charts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PES.ViewModels
+{
+    public class Charts
+    {
+        public int Month { get; set; }
+
+        public int Requests { get; set; }
+    }
+}
